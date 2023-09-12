88213d42fb90466c5779ecc451276483f919f8ad
pegasis470 <101937683+pegasis470@users.noreply.github.com>
2023-09-12 14:27:44 +0530

    Update Herbs_classification.py

--- Herbs_classification.py
+++ Herbs_classification.py
@@ -9,6 +9,52 @@ import keras.utils as image
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import shutil
+
+def Augment_data(img_path,Aug_dir):
+    aug_img=[]
+    image=Image.open(img_path)
+    image=image.resize((400,400))
+    width, height = image.size
+    aug_img.append(np.array(ImageOps.grayscale(image)))
+    aug_img.append(np.array(ImageOps.equalize(image)))
+    image = np.array(image)
+    aug_img.append(image)
+    Noise = np.random.normal(0,10, (height, width,3)).astype(np.uint8)
+    aug_img.append(image+Noise)
+    aug_img.append((-1 * image + 255).astype(np.uint8))
+    aug_img.append((5*image+255).astype(np.uint8))
+    aug_img.append((cv2.GaussianBlur(image,(5,5),sigmaX=10,sigmaY=10)).astype(np.uint8))
+    for i in range(len(aug_img)):
+        plt.imsave(f'{Aug_dir}/{img_path.split("/")[-1]}{i}.jpg',aug_img[i])
+
+folders=os.listdir('Herb')
+os.mkdir('Aug_data')
+for i in folders:
+    os.mkdir(f'Aug_data/{i}')
+
+for i in folders:
+    files=os.listdir(f'Herb/{i}')
+    for j in files:
+        Augment_data(f'Herb/{i}/{j}',f'Aug_data/{i}')
+
+
+folders=os.listdir('Aug_data')
+os.mkdir('train')
+os.mkdir('test')
+for i in folders:
+    os.mkdir(f'train/{i}')
+    os.mkdir(f'test/{i}')
+Train_files=[]
+Test_files=[]
+for i in folders:
+    all_files=os.listdir(f'Aug_data/{i}')
+    Train_files,Test_files=train_test_split(all_files,test_size=0.2)
+    for j in Train_files:
+        shutil.copy(f'Aug_data/{i}/{j}',f'train/{i}')
+    for j in Test_files:
+        shutil.copy(f'Aug_data/{i}/{j}',f'test/{i}')
+
 
 train_datagen = ImageDataGenerator(rotation_range=40,
         width_shift_range=0.2,
