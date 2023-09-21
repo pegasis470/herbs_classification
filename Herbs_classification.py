c9853eaf355d9c1309d29cef963b7390ea87aae9
pegasis470 <101937683+pegasis470@users.noreply.github.com>
2023-09-21 23:59:14 +0530

    Update Herbs_classification.py

--- Herbs_classification.py
+++ Herbs_classification.py
@@ -17,35 +17,36 @@ def plot(history):
     plt.legend([ 'train','test','train_loss','test_loss'], loc='upper left')
     plt.savefig("model_performance.png")
 
-def Model():
-    tf.keras.backend.clear_session()
-    inputs= tf.keras.Input(shape=(150,150,3))
-    convo1= tf.keras.layers.Conv2D(filters=128, kernel_size=3, activation='relu')(inputs)
-    maxpool1=tf.keras.layers.MaxPool2D(pool_size=3, strides=2)(convo1)
-    convo2= tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu')(maxpool1)
-    maxpool2=tf.keras.layers.MaxPool2D(pool_size=3, strides=2)(convo2)
-    #normalize=tf.keras.layers.BatchNormalization()(maxpool2)
-    flatten=tf.keras.layers.Flatten()(maxpool2)
-    dence1=tf.keras.layers.Dense(100,activation='relu')(flatten)
-    dence2=tf.keras.layers.Dense(75,activation='relu')(dence1)
-    dence3=tf.keras.layers.Dense(50,activation='relu')(dence2)
-    dence4=tf.keras.layers.Dense(25,activation='relu')(dence3)
-    dropout1=tf.keras.layers.Dropout(0.1)(dence4)
-    dence5=tf.keras.layers.Dense(100,activation='relu')(flatten)
-    dence6=tf.keras.layers.Dense(75,activation='relu')(dence5)
-    dence7=tf.keras.layers.Dense(50,activation='relu')(dence6)
-    dence8=tf.keras.layers.Dense(25,activation='relu')(dence7)
-    dropout2=tf.keras.layers.Dropout(0.1)(dence8)
-    #outputs=tf.keras.layers.Dense(10,activation='softmax')(dence1)
-    conc=tf.keras.layers.Concatenate()([dropout1,dropout2])
-    drop=tf.keras.layers.Dropout(0.5)(conc)
-    outputs = tf.keras.layers.Dense(10, activation='softmax')(drop)
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-    return model
-
+#def Model():
+#    tf.keras.backend.clear_session()
+#    inputs= tf.keras.Input(shape=(150,150,3))
+#    convo1= tf.keras.layers.Conv2D(filters=128, kernel_size=3, activation='relu')(inputs)
+#    maxpool1=tf.keras.layers.MaxPool2D(pool_size=3, strides=2)(convo1)
+#    convo2= tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu')(maxpool1)
+#    maxpool2=tf.keras.layers.MaxPool2D(pool_size=3, strides=2)(convo2)
+#    #normalize=tf.keras.layers.BatchNormalization()(maxpool2)
+#    flatten=tf.keras.layers.Flatten()(maxpool2)
+#    dence1=tf.keras.layers.Dense(100,activation='relu')(flatten)
+#    dence2=tf.keras.layers.Dense(75,activation='relu')(dence1)
+#    dence3=tf.keras.layers.Dense(50,activation='relu')(dence2)
+#    dence4=tf.keras.layers.Dense(25,activation='relu')(dence3)
+#    dropout1=tf.keras.layers.Dropout(0.1)(dence4)
+#    dence5=tf.keras.layers.Dense(100,activation='relu')(flatten)
+#    dence6=tf.keras.layers.Dense(75,activation='relu')(dence5)
+#    dence7=tf.keras.layers.Dense(50,activation='relu')(dence6)
+#    dence8=tf.keras.layers.Dense(25,activation='relu')(dence7)
+#    dropout2=tf.keras.layers.Dropout(0.1)(dence8)
+#    #outputs=tf.keras.layers.Dense(10,activation='softmax')(dence1)
+#    conc=tf.keras.layers.Concatenate()([dropout1,dropout2])
+#    drop=tf.keras.layers.Dropout(0.5)(conc)
+#    outputs = tf.keras.layers.Dense(10, activation='softmax')(drop)
+#    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+#    return model
+#
 folders=os.listdir('Herb')
+
 #pr.AUG()
-#pr.Train_test_split()
+pr.Train_test_split()
 datagen = ImageDataGenerator(rescale=1./255) 
 training_set = datagen.flow_from_directory('train',target_size=(150,150),
                                                  batch_size = 10
@@ -54,18 +55,28 @@ training_set = datagen.flow_from_directory('train',target_size=(150,150),
 validation_set = datagen.flow_from_directory('test',target_size=(150,150),
                                             batch_size = 10,classes=folders)
 tf.keras.backend.clear_session()
-#model = tf.keras.models.Sequential([tf.keras.Input(shape=(150,150,3)),
-#                                    tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
-#                                    tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
-#                                    tf.keras.layers.Dropout(0.1),
-#                                    tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
-#                                    tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
-#                                    tf.keras.layers.Dropout(0.1),
-#                                    tf.keras.layers.Flatten(),
-#                                    tf.keras.layers.Dense(250,activation='relu'),
-#                                    tf.keras.layers.Dense(10, activation='softmax')])
-#
-model=Model()
+model = tf.keras.models.Sequential([tf.keras.Input(shape=(150,150,3)),
+                                    tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+                                    tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
+                                    tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+                                    tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
+                                   # tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+                                   # tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
+                                   # tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+                                   # tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
+                                   # tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+                                   # tf.keras.layers.MaxPool2D(pool_size=3, strides=2),           
+                                    #tf.keras.layers.Dropout(0.1),
+                                    tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+                                    tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
+                                    #tf.keras.layers.Dropout(0.1),
+                                    tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(250,activation='relu'),
+                                    tf.keras.layers.Dense(250,activation='relu'),
+                                    tf.keras.layers.Dense(250,activation='relu'), 
+                                    tf.keras.layers.Dense(10, activation='softmax')])
+
+#model=Model()
 tf.keras.utils.plot_model(model, show_shapes=True,to_file='Model.jpg')
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=3)
 model.compile(optimizer ='adam' , loss = 'categorical_crossentropy', metrics = ['acc'])
